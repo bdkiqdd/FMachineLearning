6e68f69abba8cfa6e812101b93c9604d93655d45
EdCKiq <br008095@gmail.com>
2021-09-12 02:11:19 -0300

    Adaptando Banco

--- CAP07/Database/base_scripts.sql
+++ CAP07/Database/base_scripts.sql
@@ -1,18 +1,18 @@
-# nome_atividade,tipo,duracao,id=None
+-- nome_atividade,tipo,duracao,id=None
 Create Table if not exists webschema.atividade (
 id serial Not NULL Primary Key,
 nome_atividade VARCHAR(20),
 tipo Varchar(10),
-duracao int
+duracao_minutos int
 )
 
-# nome_aparelho,id=None
+-- nome_aparelho,id=None
 Create table if NOT EXISTS webschema.aparelho (
 id SMALLSERIAL NOT NULL Primary Key ,
 nome_aparelho Varchar(20)
 )
 
-# nome,sobrenome,data_nascimento,id=None
+-- nome,sobrenome,data_nascimento,id=None
 CREATE TABLE IF NOT EXISTS webschema.membro (
 id serial NOT NULL Primary KEY ,
 nome Varchar(15) NOT NULL,
@@ -20,7 +20,7 @@ sobrenome VArchar(25) NOT NULL,
 data_nascimento date NOT NULL
 )
  
-# nome,sobrenome,data_nascimento,id=None
+-- nome,sobrenome,data_nascimento,id=None
 CREATE TABLE IF NOT EXISTS webschema.instrutor(
 id SMALLSERIAL NOT NULL PRIMARY KEY ,
 nome VArchar(15) NOT NULL,
@@ -28,7 +28,7 @@ sobrenome VArchar(25) NOT NULL,
 data_nascimento date NOT NULL
 )
 
-# data,instrutor,atividade,perfil,id=None
+-- data,instrutor,atividade,perfil,id=None
 Create TABLE if NOT EXISTS webschema.agendamento(
 id serial NOT NULL PRIMARY KEY ,
 data date NOT NULL,
---
+++ CAP07/Database/carga.sql
@@ -0,0 +1,30 @@
+-- Atividades
+
+INSERT INTO webschema.atividades (nome_atividade,tipo,duracao_minutos) VALUES ('Zumba','Dança',40);
+INSERT INTO webschema.atividades (nome_atividade,tipo,duracao_minutos) VALUES ('Boxe','Luta',50);
+INSERT INTO webschema.atividades (nome_atividade,tipo,duracao_minutos) VALUES ('Bicicleta','Força',120);
+
+-- Nome do Aparelho
+
+INSERT INTO webschema.aparelho (nome_aparelho) VALUES ('Bicicleta');
+INSERT INTO webschema.aparelho (nome_aparelho) VALUES ('Pula Pula');
+INSERT INTO webschema.aparelho (nome_aparelho) VALUES ('Caixa de Som');
+
+-- Membro
+
+INSERT INTO webschema.membro (nome,sobrenome,data_nascimento) VALUES ('Claudio','Peres', '1994-08-25');
+INSERT INTO webschema.membro (nome,sobrenome,data_nascimento) VALUES ('Henrique','Ferreira','1990-12-25');
+INSERT INTO webschema.membro (nome,sobrenome,data_nascimento) VALUES ('Kaique','Viana','2002-09-24');
+
+-- Instrutor
+
+INSERT INTO webschema.instrutor (nome,sobrenome,data_nascimento) VALUES ('Claudio','Peres','1994-08-25');
+INSERT INTO webschema.instrutor (nome,sobrenome,data_nascimento) VALUES ('Fabio','Silva','1991-08-25');
+INSERT INTO webschema.instrutor (nome,sobrenome,data_nascimento) VALUES ('Ferreira','Pinto','2000-10-12');
+
+-- Agendamento
+
+INSERT INTO webschema.agendamento (data,instrutor,atividade,perfil) VALUES ('2021-10-29',2,8,3);
+INSERT INTO webschema.agendamento (data,instrutor,atividade,perfil) VALUES ('2021-10-31',1,7,2);
+INSERT INTO webschema.agendamento (data,instrutor,atividade,perfil) VALUES ('2021-10-27',3,9,1);
+INSERT INTO webschema.agendamento (data,instrutor,atividade,perfil) VALUES ('2021-10-15',2,9,3);
